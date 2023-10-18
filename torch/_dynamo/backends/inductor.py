98b61f39d675d5ad1f5994eb9b53957ef2535d16
Stella Laurenzo <stellaraccident@gmail.com>
2023-10-17 21:29:02 -0700

    Apply lint patch

--- torch/_dynamo/backends/inductor.py
+++ torch/_dynamo/backends/inductor.py
@@ -1,4 +1,5 @@
 import sys
+
 from torch._dynamo import register_backend
 
 
@@ -6,7 +7,7 @@ from torch._dynamo import register_backend
 def inductor(*args, **kwargs):
     if sys.platform == "win32":
         raise RuntimeError("Windows not yet supported for inductor")
-        
+
     # do import here to avoid loading inductor into memory when it is not used
     from torch._inductor.compile_fx import compile_fx
 
