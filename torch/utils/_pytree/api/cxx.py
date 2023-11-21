5c2bf07966d3ad8431490b18e39db0324f1253d0
Xuehai Pan <XuehaiPan@pku.edu.cn>
2023-11-22 00:16:42 +0800

    [pytree] reorganize submodule structure for C++ and Python pytree
    ghstack-source-id: 90c9822f806f8a360ec9b7b71837f0d176b31a1b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112278

--- torch/utils/_pytree/api/cxx.py
+++ torch/utils/_pytree/api/cxx.py
@@ -14,18 +14,7 @@ collection support for PyTorch APIs.
 
 import functools
 import warnings
-from typing import (
-    Any,
-    Callable,
-    Iterable,
-    List,
-    Optional,
-    overload,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-)
+from typing import Any, Callable, Iterable, List, Optional, overload, Tuple, Type, Union
 
 import torch
 
@@ -34,6 +23,20 @@ if torch._running_with_deploy():
 
 import optree
 from optree import PyTreeSpec  # direct import for type annotations
+
+from .typing import (
+    Context,
+    DumpableContext,
+    FlattenFunc,
+    FromDumpableContextFn,
+    PyTree,
+    R,
+    S,
+    T,
+    ToDumpableContextFn,
+    U,
+    UnflattenFunc,
+)
 
 
 __all__ = [
@@ -65,21 +68,8 @@ __all__ = [
 ]
 
 
-T = TypeVar("T")
-S = TypeVar("S")
-U = TypeVar("U")
-R = TypeVar("R")
-
-
-Context = Optional[Any]
-PyTree = Any
 TreeSpec = PyTreeSpec
-FlattenFunc = Callable[[PyTree], Tuple[List, Context]]
-UnflattenFunc = Callable[[Iterable, Context], PyTree]
 OpTreeUnflattenFunc = Callable[[Context, Iterable], PyTree]
-DumpableContext = Any  # Any json dumpable text
-ToDumpableContextFn = Callable[[Context], DumpableContext]
-FromDumpableContextFn = Callable[[DumpableContext], Context]
 
 
 def _reverse_args(func: UnflattenFunc) -> OpTreeUnflattenFunc:
@@ -224,7 +214,7 @@ def register_pytree_node(
         namespace=namespace,
     )
 
-    from . import _pytree as python
+    from . import python
 
     python._private_register_pytree_node(
         cls,
@@ -361,8 +351,8 @@ def _register_pytree_node(
         )
     """
     warnings.warn(
-        "torch.utils._cxx_pytree._register_pytree_node is deprecated. "
-        "Please use torch.utils._cxx_pytree.register_pytree_node instead.",
+        "torch.utils._pytree.api.cxx._register_pytree_node is deprecated. "
+        "Please use torch.utils._pytree.api.cxx.register_pytree_node instead.",
         stacklevel=2,
     )
 
@@ -1032,7 +1022,7 @@ def treespec_dumps(treespec: TreeSpec) -> str:
             f"treespec_dumps(spec): Expected `spec` to be instance of "
             f"TreeSpec but got item of type {type(treespec)}."
         )
-    from ._pytree import (
+    from .python import (
         tree_structure as _tree_structure,
         treespec_dumps as _treespec_dumps,
     )
@@ -1043,7 +1033,7 @@ def treespec_dumps(treespec: TreeSpec) -> str:
 
 def treespec_loads(serialized: str) -> TreeSpec:
     """Deserialize a treespec from a JSON string."""
-    from ._pytree import (
+    from .python import (
         tree_unflatten as _tree_unflatten,
         treespec_loads as _treespec_loads,
     )
