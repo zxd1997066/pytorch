69d46427a45625e6520f1f502bebcda1e67b19e5
Kai Londenberg <klondenberg@meta.com>
2023-12-07 09:28:23 +0100

    [Inductor max autotune] Multithreaded Precompilation
    When using the Cutlass backend, the compilation
    of CUDA source files can totally dominate the runtime required for the benchmarking done
    as part of Autotuning.
    
    This change adds a multithreaded precompilation phase, which serves to pre-populate the compilation cache ( both in-memory, and a
    possible on-disk sccache ).
    
    Also it ensures that no unneccessary compilation
    and benchmarking steps are performed, which was peviously the case.
    
    ghstack-source-id: 1348f2d8df10c0c9f75f143461b5a82166f486bb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113558

--- torch/_inductor/codegen/cuda/cuda_kernel.py
+++ torch/_inductor/codegen/cuda/cuda_kernel.py
@@ -298,7 +298,7 @@ class CUDATemplateCaller(ChoiceCaller):
         layout: Layout,
         make_kernel_render: Callable[[CUDATemplateBuffer, Optional[List[IRNode]]], str],
         bmreq: CUDABenchmarkRequest,
-        template: "CUDATemplate",  # type: ignore[name-defined],
+        template: "CUDATemplate",  # type: ignore[name-defined]
         info_kwargs: Optional[dict[str, PrimitiveInfoType]],  # type: ignore[type-arg]
     ):
         super().__init__(name, input_nodes, layout)
@@ -307,6 +307,10 @@ class CUDATemplateCaller(ChoiceCaller):
         self.bmreq = bmreq
         self.template = template
         self.info_kwargs = info_kwargs
+
+    def precompile(self) -> None:
+        assert self.bmreq is not None
+        self.bmreq.precompile()
 
     def benchmark(self, *args, out) -> float:
         assert self.bmreq is not None
@@ -340,7 +344,9 @@ class CUDATemplateCaller(ChoiceCaller):
                 "kernel_schedule": str(op.kernel_schedule),
                 "element_accumulator": str(op.accumulator_type()),
                 "op_name": str(op.procedural_name()),
-                "instruction_shape" : str(op.tile_description.math_instruction.instruction_shape),
+                "instruction_shape": str(
+                    op.tile_description.math_instruction.instruction_shape
+                ),
             }
         else:
             return {"backend": "CUDA", "op_type": "unknown"}
