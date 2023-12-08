c4c475d06e33cbde3e687f0bb8bd6382c2c37824
YuqingJ <33777788+YuqingJ@users.noreply.github.com>
2023-12-08 15:55:42 -0800

    Add back non standard shapes test samples for SDPA in common_methods_invocations
    These samples probably were accidentally dropped in the older PR

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -8425,6 +8425,7 @@ def sample_inputs_scaled_dot_product_attention(op_info, device, dtype, requires_
         is_causal=is_causal,
         dropout_p=dropout_p
     )
+    samples.append(diff_v_head_dim)
 
     # Add an attn_mask
     samples.append(
@@ -8489,7 +8490,8 @@ def sample_inputs_efficient_attention_forward(op_info, device, dtype, requires_g
         causal_diagonal=None,
         seqlen_k=None
     )
-
+    samples.append(diff_v_head_dim)
+    
     # Add an attn_mask
     samples.append(
         SampleInput(
