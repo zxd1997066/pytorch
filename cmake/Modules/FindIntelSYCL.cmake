920623721acb37599684079a58482f054c1d4328
Yu, Guangye <guangye.yu@intel.com>
2023-12-18 16:20:06 +0000

    refine cmake

--- cmake/Modules/FindIntelSYCL.cmake
+++ cmake/Modules/FindIntelSYCL.cmake
@@ -16,16 +16,9 @@
 
 # This will define the following variables:
 # IntelSYCL_FOUND          : True if the system has the SYCL library.
-# SYCL_LANGUAGE_VERSION    : The SYCL language spec version by compiler.
 # SYCL_INCLUDE_DIR         : Include directories needed to use SYCL.
 # SYCL_LIBRARY_DIR         ：The path to the SYCL library.
 # SYCL_LIBRARY             : SYCL library fullname.
-
-# The following cache variables may also be set:
-# SYCL_LANGUAGE_VERSION
-# SYCL_INCLUDE_DIR
-# SYCL_LIBRARY_DIR
-# SYCL_LIBRARY
 
 include(FindPackageHandleStandardArgs)
 
@@ -44,101 +37,11 @@ if(nosyclfound)
     return()
 endif()
 
-# Assume that CXX Compiler supports SYCL and then test to verify.
-set(SYCL_COMPILER ${INTEL_SYCL_ROOT}/bin/icpx)
-
-# Function to write a test case to verify SYCL features.
-function(SYCL_FEATURE_TEST_WRITE src)
-  set(pp_if "#if")
-  set(pp_endif "#endif")
-
-  set(SYCL_TEST_CONTENT "")
-  string(APPEND SYCL_TEST_CONTENT "#include <iostream>\nusing namespace std;\n")
-  string(APPEND SYCL_TEST_CONTENT "int main(){\n")
-
-  # Feature tests goes here
-  string(APPEND SYCL_TEST_CONTENT "${pp_if} defined(SYCL_LANGUAGE_VERSION)\n")
-  string(APPEND SYCL_TEST_CONTENT "cout << \"SYCL_LANGUAGE_VERSION=\"<<SYCL_LANGUAGE_VERSION<<endl;\n")
-  string(APPEND SYCL_TEST_CONTENT "${pp_endif}\n")
-
-  string(APPEND SYCL_TEST_CONTENT "return 0;}\n")
-
-  file(WRITE ${src} "${SYCL_TEST_CONTENT}")
-endfunction()
-
-# Function to build the feature check test case.
-function(SYCL_FEATURE_TEST_BUILD TEST_SRC_FILE TEST_EXE)
-  # Convert CXX Flag string to list
-  set(SYCL_CXX_FLAGS_LIST "${SYCL_CXX_FLAGS}")
-  separate_arguments(SYCL_CXX_FLAGS_LIST)
-
-  # Spawn a process to build the test case.
-  execute_process(
-    COMMAND ${SYCL_COMPILER}
-    ${SYCL_CXX_FLAGS_LIST}
-    ${TEST_SRC_FILE}
-    "-o"
-    ${TEST_EXE}
-    WORKING_DIRECTORY ${SYCL_TEST_DIR}
-    OUTPUT_VARIABLE output ERROR_VARIABLE output
-    OUTPUT_FILE ${SYCL_TEST_DIR}/Compile.log
-    RESULT_VARIABLE result
-    TIMEOUT 20
-    )
-
-  # Verify if test case build properly.
-  if(result)
-    message("SYCL feature test compile failed!")
-    message("compile output is: ${output}")
-  endif()
-endfunction()
-
-# Function to run the test case to generate feature info.
-function(SYCL_FEATURE_TEST_RUN TEST_EXE)
-  # Spawn a process to run the test case.
-  execute_process(
-    COMMAND ${TEST_EXE}
-    WORKING_DIRECTORY ${SYCL_TEST_DIR}
-    OUTPUT_VARIABLE output ERROR_VARIABLE output
-    RESULT_VARIABLE result
-    TIMEOUT 20
-    )
-
-  # Verify the test execution output.
-  if(test_result)
-    set(IntelSYCL_FOUND False)
-    set(SYCL_REASON_FAILURE "SYCL feature test execution failed!!")
-  endif()
-
-  set( test_result "${result}" PARENT_SCOPE)
-  set( test_output "${output}" PARENT_SCOPE)
-endfunction()
-
-# Function to extract the information from test execution.
-function(SYCL_FEATURE_TEST_EXTRACT test_output)
-  string(REGEX REPLACE "\n" ";" test_output_list "${test_output}")
-
-  set(SYCL_LANGUAGE_VERSION "")
-  foreach(strl ${test_output_list})
-     if(${strl} MATCHES "^SYCL_LANGUAGE_VERSION=([A-Za-z0-9_]+)$")
-       string(REGEX REPLACE "^SYCL_LANGUAGE_VERSION=" "" extracted_sycl_lang "${strl}")
-       set(SYCL_LANGUAGE_VERSION ${extracted_sycl_lang})
-     endif()
-  endforeach()
-
-  set(SYCL_LANGUAGE_VERSION "${SYCL_LANGUAGE_VERSION}" PARENT_SCOPE)
-endfunction()
-
-# use REALPATH to resolve symlinks.
-get_filename_component(_REALPATH_SYCL_COMPILER "${SYCL_COMPILER}" REALPATH)
-get_filename_component(SYCL_BIN_DIR "${_REALPATH_SYCL_COMPILER}" DIRECTORY)
-get_filename_component(SYCL_PACKAGE_DIR "${SYCL_BIN_DIR}" DIRECTORY CACHE)
-
 # Find include path from binary.
 find_file(
   SYCL_INCLUDE_DIR
   NAMES include
-  HINTS ${SYCL_PACKAGE_DIR}
+  HINTS ${INTEL_SYCL_ROOT}
   NO_DEFAULT_PATH
   )
 
@@ -146,7 +49,7 @@ find_file(
 find_file(
   SYCL_INCLUDE_SYCL_DIR
   NAMES sycl
-  HINTS ${SYCL_PACKAGE_DIR}/include/
+  HINTS ${INTEL_SYCL_ROOT}/include/
   NO_DEFAULT_PATH
   )
 
@@ -157,7 +60,7 @@ list(APPEND SYCL_INCLUDE_DIR ${SYCL_INCLUDE_SYCL_DIR})
 find_file(
   SYCL_LIBRARY_DIR
   NAMES lib lib64
-  HINTS ${SYCL_PACKAGE_DIR}
+  HINTS ${INTEL_SYCL_ROOT}
   NO_DEFAULT_PATH
   )
 
@@ -176,54 +79,8 @@ if((NOT SYCL_INCLUDE_DIR) OR (NOT SYCL_LIBRARY_DIR) OR (NOT SYCL_LIBRARY))
   return()
 endif()
 
-set(SYCL_FLAGS "-fsycl")
-# Windows: Add exception handling
-if(WIN32)
-  set(SYCL_FLAGS "${SYCL_FLAGS} /EHsc")
-endif()
-
-set(SYCL_CXX_FLAGS "${SYCL_FLAGS}")
-
-# And now test the assumptions.
-# Create a clean working directory.
-set(SYCL_TEST_DIR "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/FindIntelSYCL")
-file(REMOVE_RECURSE ${SYCL_TEST_DIR})
-file(MAKE_DIRECTORY ${SYCL_TEST_DIR})
-
-# Create the test source file.
-set(TEST_SRC_FILE "${SYCL_TEST_DIR}/sycl_features.cpp")
-set(TEST_EXE "${TEST_SRC_FILE}.exe")
-SYCL_FEATURE_TEST_WRITE(${TEST_SRC_FILE})
-
-# Build the test and create test executable.
-SYCL_FEATURE_TEST_BUILD(${TEST_SRC_FILE} ${TEST_EXE})
-
-# Execute the test to extract information.
-SYCL_FEATURE_TEST_RUN(${TEST_EXE})
-
-# Extract test output for information.
-SYCL_FEATURE_TEST_EXTRACT(${test_output})
-
-# As per specification, all the SYCL compatible compilers should define macro
-# SYCL_LANGUAGE_VERSION.
-string(COMPARE EQUAL "${SYCL_LANGUAGE_VERSION}" "" nosycllang)
-if(nosycllang)
-  set(IntelSYCL_FOUND False)
-  set(SYCL_REASON_FAILURE "It appears that the ${SYCL_COMPILER} does not support SYCL!!")
-  set(IntelSYCL_NOT_FOUND_MESSAGE "${SYCL_REASON_FAILURE}")
-endif()
-
-message(STATUS "The SYCL compiler is ${SYCL_COMPILER}")
-message(STATUS "The SYCL Language Version is ${SYCL_LANGUAGE_VERSION}")
-
 find_package_handle_standard_args(
   IntelSYCL
   FOUND_VAR IntelSYCL_FOUND
-  REQUIRED_VARS SYCL_COMPILER SYCL_LANGUAGE_VERSION SYCL_INCLUDE_DIR SYCL_LIBRARY_DIR SYCL_LIBRARY
+  REQUIRED_VARS SYCL_INCLUDE_DIR SYCL_LIBRARY_DIR SYCL_LIBRARY
   REASON_FAILURE_MESSAGE "${SYCL_REASON_FAILURE}")
-
-# Include in cache.
-set(SYCL_LANGUAGE_VERSION "${SYCL_LANGUAGE_VERSION}" CACHE STRING "SYCL Language version")
-set(SYCL_INCLUDE_DIR "${SYCL_INCLUDE_DIR}" CACHE FILEPATH "SYCL Include directory")
-set(SYCL_LIBRARY_DIR "${SYCL_LIBRARY_DIR}" CACHE FILEPATH "SYCL Library Directory")
-set(SYCL_LIBRARY "${SYCL_LIBRARY}" CACHE STRING "SYCL Library Fullname")
