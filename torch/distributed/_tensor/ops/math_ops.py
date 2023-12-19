d5caf14d0c46dc6a686a1212212a796056516020
Xilun Wu <12968408+XilunWu@users.noreply.github.com>
2023-12-19 08:52:35 -0800

    [DTensor] add layer norm backward support
    ghstack-source-id: 55344d0edb382ecfc8f524ac7572f76d2a80768c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/115683
    
    [DTensor] change backward layer norm return from TupleStrategy to OpStrategy
    
    [DTensor] add layer norm backward test check on extra comms

--- torch/distributed/_tensor/ops/math_ops.py
+++ torch/distributed/_tensor/ops/math_ops.py
@@ -354,6 +354,178 @@ def layer_norm_strategy(mesh: DeviceMesh, op_schema: OpSchema) -> OpStrategy:
     return output_strategy
 
 
+@register_op_strategy(
+    [aten.native_layer_norm_backward.default],
+    schema_info=RuntimeSchemaInfo(2),
+)
+def layer_norm_bwd_strategy(mesh: DeviceMesh, op_schema: OpSchema) -> OpStrategy:
+    # args must be: grad_out, input, normalized_shape, mean, rstd,
+    # weight, bias, output_mask. For None weight and bias, their
+    # corresponding objects will be None as well.
+    assert len(op_schema.args_schema) == 8
+    (
+        grad_out_strategy,
+        input_strategy,
+        normalized_shape,
+        mean_strategy,
+        rstd_strategy,
+        weight_strategy,
+        bias_strategy,
+        output_mask,
+    ) = op_schema.args_schema
+
+    assert isinstance(grad_out_strategy, OpStrategy)
+    assert isinstance(input_strategy, OpStrategy)
+    assert isinstance(mean_strategy, OpStrategy)
+    assert isinstance(rstd_strategy, OpStrategy)
+
+    assert isinstance(normalized_shape, (int, Sequence, torch.Size))
+    normalized_size = normalize_to_torch_size(normalized_shape)
+    input_ndim = input_strategy.output_ndim
+    axis = input_ndim - len(normalized_size)
+    outer_dims = list(range(axis))
+
+    assert isinstance(output_mask, List) and len(output_mask) == 3
+
+    # output triple: (d_input, d_weight, d_bias)
+    out_tuple_strategy = OpStrategy([])
+    for idx, _ in enumerate(input_strategy.strategies):
+        output_specs_list: List[Optional[DTensorSpec]] = []
+        op_args_target_specs = []
+        redistribute_costs = []
+        input_src_spec = input_strategy.strategies[idx].output_spec
+        assert isinstance(input_src_spec, DTensorSpec)
+
+        # arg: grad_out
+        # TODO: change the strategy to the following rule.
+        # d_input is basically a product of element-wise mul of
+        # grad_out, rstd, and normalized input, among which rstd
+        # and normalized input (x_hat) should have the same sharding
+        # placements, and grad_out's sharding is determined by the
+        # pointwise result of x_hat and weight/bias.
+        if output_mask[0]:
+            # TODO: now grad_out spec follows input spec. we may need
+            # to change it to apply a pointwise rule over grad_out,
+            # input, and weight.
+            grad_out_target_spec = DTensorSpec(
+                mesh=mesh,
+                placements=_replicate_dims_start_at(input_src_spec.placements, axis),
+                tensor_meta=input_src_spec.tensor_meta,
+            )
+            op_args_target_specs.append(grad_out_target_spec)
+            redistribute_costs.append(
+                generate_redistribute_costs(grad_out_strategy, grad_out_target_spec)
+            )
+            output_specs_list.append(grad_out_target_spec)
+            out_tuple_strategy.strategies.append(
+                # these 3 lists will be filled in later
+                PlacementStrategy(
+                    output_spec=output_specs_list,
+                    input_specs=op_args_target_specs,
+                    redistribute_cost=redistribute_costs,
+                )
+            )
+        else:
+            output_specs_list.append(None)
+
+        # arg: input
+        input_target_spec = DTensorSpec(
+            mesh=mesh,
+            placements=_replicate_dims_start_at(input_src_spec.placements, axis),
+            tensor_meta=input_src_spec.tensor_meta,
+        )
+        op_args_target_specs.append(input_target_spec)
+        redistribute_costs.append(
+            generate_redistribute_costs(input_strategy, input_target_spec)
+        )
+
+        # arg: mean, rstd
+        mean_target_spec = mean_strategy.strategies[idx].output_spec
+        assert isinstance(mean_target_spec, DTensorSpec)
+        op_args_target_specs.append(mean_target_spec)
+        redistribute_costs.append(
+            generate_redistribute_costs(mean_strategy, mean_target_spec)
+        )
+        rstd_target_spec = rstd_strategy.strategies[idx].output_spec
+        assert isinstance(rstd_target_spec, DTensorSpec)
+        op_args_target_specs.append(rstd_target_spec)
+        redistribute_costs.append(
+            generate_redistribute_costs(rstd_strategy, rstd_target_spec)
+        )
+
+        # arg: weight
+        # d_weight = sum(grad_out * (input - mean) / rstd, outer_dim, keepdim=False)
+        if output_mask[1]:
+            assert isinstance(weight_strategy, OpStrategy)
+            weight_src_spec = weight_strategy.strategies[idx].output_spec
+            assert isinstance(weight_src_spec, DTensorSpec)
+            weight_target_spec = DTensorSpec(
+                mesh=mesh,
+                placements=_replicate_dims_start_at(weight_src_spec.placements),
+                tensor_meta=weight_src_spec.tensor_meta,
+            )
+            op_args_target_specs.append(weight_target_spec)
+            redistribute_costs.append(
+                generate_redistribute_costs(weight_strategy, weight_target_spec)
+            )
+            # weight_grad_spec = weight_target_spec  # TODO
+            # TODO: now d_weight spec follows input spec w/ a reduction.
+            # we may need to change to a pointwise rule over grad_out and
+            # input, then apply a reduction.
+            inp_placements = _replicate_dims_start_at(input_src_spec.placements, axis)
+            reduce_dims_map = _infer_reduce_dims_map(
+                outer_dims, input_src_spec.ndim, False
+            )
+            out_placements = map_placements_after_reduction(
+                inp_placements, outer_dims, reduce_dims_map, c10d.ReduceOp.SUM
+            )
+            output_specs_list.append(
+                DTensorSpec(
+                    mesh=mesh,
+                    placements=out_placements,
+                    tensor_meta=weight_src_spec.tensor_meta,
+                )
+            )
+        else:
+            output_specs_list.append(None)
+
+        # arg: bias
+        # d_bias = sum(grad_out, outer_dim, keepdim=False)
+        if output_mask[2]:
+            assert isinstance(bias_strategy, OpStrategy)
+            bias_src_spec = bias_strategy.strategies[idx].output_spec
+            assert isinstance(bias_src_spec, DTensorSpec)
+            bias_target_spec = DTensorSpec(
+                mesh=mesh,
+                placements=_replicate_dims_start_at(bias_src_spec.placements),
+                tensor_meta=bias_src_spec.tensor_meta,
+            )
+            op_args_target_specs.append(bias_target_spec)
+            redistribute_costs.append(
+                generate_redistribute_costs(bias_strategy, bias_target_spec)
+            )
+            grad_out_spec = output_specs_list[0]
+            assert isinstance(grad_out_spec, DTensorSpec)
+            inp_placements = _replicate_dims_start_at(grad_out_spec.placements, axis)
+            reduce_dims_map = _infer_reduce_dims_map(
+                outer_dims, grad_out_spec.ndim, False
+            )
+            out_placements = map_placements_after_reduction(
+                inp_placements, outer_dims, reduce_dims_map, c10d.ReduceOp.SUM
+            )
+            output_specs_list.append(
+                DTensorSpec(
+                    mesh=mesh,
+                    placements=out_placements,
+                    tensor_meta=bias_src_spec.tensor_meta,
+                )
+            )
+        else:
+            output_specs_list.append(None)
+
+    return out_tuple_strategy
+
+
 def _replicate_dims_start_at(
     placements: Sequence[Placement], start_dim: int = 0
 ) -> Tuple[Placement, ...]:
--- torch/distributed/_tensor/sharding_prop.py
+++ torch/distributed/_tensor/sharding_prop.py
@@ -71,7 +71,9 @@ class ShardingPropagator:
 
     def _propagate_tensor_meta(
         self, op_schema: OpSchema
-    ) -> Union[None, TensorMeta, List[TensorMeta], Tuple[TensorMeta, ...]]:
+    ) -> Union[
+        None, TensorMeta, List[Optional[TensorMeta]], Tuple[Optional[TensorMeta], ...]
+    ]:
         """
         Propagate the tensor metadata, it could either return a TensorMeta
         or a list/tuple of TensorMetas
@@ -93,7 +95,7 @@ class ShardingPropagator:
             )
 
         elif isinstance(fake_out, (tuple, list)):
-            tensor_meta_list = []
+            tensor_meta_list: List[Optional[TensorMeta]] = []
             for fake_out_item in fake_out:
                 if isinstance(fake_out_item, torch.Tensor):
                     tensor_meta_list.append(
@@ -103,6 +105,8 @@ class ShardingPropagator:
                             dtype=fake_out_item.dtype,
                         )
                     )
+                else:
+                    tensor_meta_list.append(None)
             return (
                 tuple(tensor_meta_list)
                 if isinstance(fake_out, tuple)
@@ -117,7 +121,10 @@ class ShardingPropagator:
         op: OpOverload,
         output_spec: OutputSpecType,
         output_tensor_meta: Union[
-            None, TensorMeta, List[TensorMeta], Tuple[TensorMeta, ...]
+            None,
+            TensorMeta,
+            List[Optional[TensorMeta]],
+            Tuple[Optional[TensorMeta], ...],
         ],
     ) -> None:
         """
@@ -279,12 +286,19 @@ class ShardingPropagator:
                 )
             elif isinstance(op_strategy, TupleStrategy):
                 # tuple strategy output sharding
-                out_spec_list: List[DTensorSpec] = []
+                input_specs_list = None
+                out_spec_list: List[Optional[DTensorSpec]] = []
                 for strategy in op_strategy.childs:
-                    assert isinstance(strategy, OpStrategy)
-                    output_strategy = self._select_strategy(strategy)
-                    assert isinstance(output_strategy.output_spec, DTensorSpec)
-                    out_spec_list.append(output_strategy.output_spec)
+                    if isinstance(strategy, OpStrategy):
+                        output_strategy = self._select_strategy(strategy)
+                        # we expect the out strategies all share the same input specs
+                        if not input_specs_list:
+                            input_specs_list = output_strategy.input_specs
+                        assert isinstance(output_strategy.output_spec, DTensorSpec)
+                        out_spec_list.append(output_strategy.output_spec)
+                    else:
+                        # for None output in tuple, its output spec should also be None
+                        out_spec_list.append(None)
 
                 needs_redistribute = False
                 suggestion_args: List[object] = []
@@ -294,9 +308,12 @@ class ShardingPropagator:
                     ):
                         expected_input_spec_list = []
                         for idx, arg_spec in enumerate(arg):
-                            if arg_spec.placements != out_spec_list[idx].placements:
+                            out_spec = out_spec_list[idx]
+                            # we expect ops whose op_type is 0 have no None output
+                            assert isinstance(out_spec, DTensorSpec)
+                            if arg_spec.placements != out_spec.placements:
                                 needs_redistribute = True
-                            expected_input_spec_list.append(out_spec_list[idx])
+                            expected_input_spec_list.append(out_spec)
                         suggestion_args.append(
                             tuple(expected_input_spec_list)
                             if isinstance(arg, tuple)
@@ -304,6 +321,8 @@ class ShardingPropagator:
                         )
                     elif isinstance(arg, DTensorSpec):
                         expected_input_spec = out_spec_list[0]
+                        # we expect ops whose op_type is 0 have no None output
+                        assert isinstance(expected_input_spec, DTensorSpec)
                         if arg.placements != expected_input_spec.placements:
                             needs_redistribute = True
                         suggestion_args.append(expected_input_spec)
