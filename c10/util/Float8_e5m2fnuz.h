5ef7ec1f81398b38853bc920943816c2d200653b
Jeff Daily <jeff.daily@amd.com>
2023-12-21 18:49:17 +0000

    lintrunner

--- c10/util/Float8_e5m2fnuz.h
+++ c10/util/Float8_e5m2fnuz.h
@@ -1,8 +1,8 @@
 #pragma once
 
-/// Defines the Float8_e5m2fnuz type (8-bit floating-point) including conversions
-/// to standard C types and basic arithmetic operations. Note that arithmetic
-/// operations are implemented by converting to floating point and
+/// Defines the Float8_e5m2fnuz type (8-bit floating-point) including
+/// conversions to standard C types and basic arithmetic operations. Note that
+/// arithmetic operations are implemented by converting to floating point and
 /// performing the operation in float32.
 /// Binary configuration remains the same as e5m2:
 /// s eeeee mm
@@ -390,14 +390,17 @@ struct alignas(1) Float8_e5m2fnuz {
 
   Float8_e5m2fnuz() = default;
 
-  constexpr C10_HOST_DEVICE Float8_e5m2fnuz(uint8_t bits, from_bits_t) : x(bits){};
+  constexpr C10_HOST_DEVICE Float8_e5m2fnuz(uint8_t bits, from_bits_t)
+      : x(bits){};
   inline C10_HOST_DEVICE Float8_e5m2fnuz(float value);
   inline C10_HOST_DEVICE operator float() const;
   inline C10_HOST_DEVICE bool isnan() const;
   inline C10_HOST_DEVICE bool isinf() const;
 };
 
-C10_API std::ostream& operator<<(std::ostream& out, const Float8_e5m2fnuz& value);
+C10_API std::ostream& operator<<(
+    std::ostream& out,
+    const Float8_e5m2fnuz& value);
 
 } // namespace c10
 
