2ae47e98f8a28ce59866bc89d69c0c84d3e0c72f
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2023-12-21 17:31:13 -0800

    Add config to disable TransformerEncoder/MHA fastpath
    ghstack-source-id: 853fd840a191ebe9b8cea104bb5124680da77400
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/112212

---
+++ torch/backends/mha/__init__.py
@@ -0,0 +1,18 @@
+# Config options to enable/disable C++ kernel for nn.functional.MHA
+# and nn.TransformerEncoder
+import torch
+
+_is_fastpath_enabled: bool = True
+
+
+def get_fastpath_enabled() -> bool:
+    """Returns whether fast path is enabled, or ``True`` if jit is scripting."""
+    if not torch.jit.is_scripting():
+        return _is_fastpath_enabled
+    return True
+
+
+def set_fastpath_enabled(value: bool) -> None:
+    """Sets whether fast path is enabled"""
+    global _is_fastpath_enabled
+    _is_fastpath_enabled = value
