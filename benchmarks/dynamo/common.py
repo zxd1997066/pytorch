b34785a7b926782b7b00e92e43a2e0383f6ea4c3
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-24 22:55:38 +0800

    Update common.py

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -2668,7 +2668,7 @@ class BenchmarkRunner:
             eager_latency, eager_peak_mem, _ = warmup(
                 self.model_iter_fn, model, example_inputs, "eager"
             )
-
+            print(eager_latency)
             if self.args.export_aot_inductor:
                 t_0 = time.perf_counter()
                 optimized_model_iter_fn = optimize_ctx
@@ -2678,10 +2678,10 @@ class BenchmarkRunner:
                 optimized_model_iter_fn = optimize_ctx(self.model_iter_fn)
                 aot_compilation_time = 0
 
-            with maybe_enable_compiled_autograd(self.args.compiled_autograd):
-                dynamo_latency, dynamo_peak_mem, dynamo_stats = warmup(
-                    optimized_model_iter_fn, model, example_inputs, "dynamo"
-                )
+            # with maybe_enable_compiled_autograd(self.args.compiled_autograd):
+            #     dynamo_latency, dynamo_peak_mem, dynamo_stats = warmup(
+            #         optimized_model_iter_fn, model, example_inputs, "dynamo"
+            #     )
 
             if self.args.profile_dynamo_cache_lookup:
                 with torch.profiler.profile(
