05b77861e80ac73055b4f946c6a1f8ed7400e4d3
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2026-03-17 10:56:04 +0800

    Update test_dtensor_compile.py

--- test/distributed/tensor/test_dtensor_compile.py
+++ test/distributed/tensor/test_dtensor_compile.py
@@ -286,7 +286,7 @@ def forward(self, L_self_buffers_buffer_ : torch.distributed.tensor.DTensor, L_x
             str(backend.fw_graphs[0].code).strip(),
             f"""\
 def forward(self, arg0_1, arg1_1, arg2_1):
-    _to_copy = torch.ops.aten._to_copy.default(arg1_1, dtype = torch.float64, layout = torch.strided, device = device(type={self.device_type}, index=0));  arg1_1 = None
+    _to_copy = torch.ops.aten._to_copy.default(arg1_1, dtype = torch.float64, layout = torch.strided, device = device(type='{self.device_type}', index=0));  arg1_1 = None
     view = torch.ops.aten.view.default(_to_copy, [4, 4]);  _to_copy = None
     add = torch.ops.aten.add.Tensor(arg0_1, view);  arg0_1 = view = None
     view_1 = torch.ops.aten.view.default(add, [4, 4]);  add = None
@@ -347,7 +347,7 @@ def forward(self, args_0):
             str(joint_gm.code).strip(),
             f"""\
 def forward(self, arg0_1, arg1_1):
-    _to_copy = torch.ops.aten._to_copy.default(arg1_1, dtype = torch.float64, layout = torch.strided, device = device(type={self.device_type}, index=0));  arg1_1 = None
+    _to_copy = torch.ops.aten._to_copy.default(arg1_1, dtype = torch.float64, layout = torch.strided, device = device(type='{self.device_type}', index=0));  arg1_1 = None
     view = torch.ops.aten.view.default(_to_copy, [4, 4]);  _to_copy = None
     add = torch.ops.aten.add.Tensor(arg0_1, view);  arg0_1 = view = None
     view_1 = torch.ops.aten.view.default(add, [4, 4]);  add = None
