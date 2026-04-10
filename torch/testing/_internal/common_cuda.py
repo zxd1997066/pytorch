58592319cc8bb823431f3e81828b76b3048924f4
intel <madhumitha.reddy.karri@intel.com>
2026-04-10 14:48:41 -0700

    Enable other skip tests in test/distributed/test_c10d_xccl.py

--- torch/testing/_internal/common_cuda.py
+++ torch/testing/_internal/common_cuda.py
@@ -15,7 +15,7 @@ import unittest
 CUDA_ALREADY_INITIALIZED_ON_IMPORT = torch.cuda.is_initialized()
 
 
-TEST_MULTIGPU = TEST_CUDA and torch.cuda.device_count() >= 2
+TEST_MULTIGPU = (TEST_CUDA or TEST_XPU) and torch.accelerator.device_count() >= 2
 CUDA_DEVICE = torch.device("cuda:0") if TEST_CUDA else None
 # note: if ROCm is targeted, TEST_CUDNN is code for TEST_MIOPEN
 if TEST_WITH_ROCM:
